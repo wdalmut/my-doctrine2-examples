96f075702da1251c1c2e4a00568978944ac3230d
Walter Dal Mut <walter.dalmut@gmail.com>
2013-02-11 22:29:08 +0100

    Spaces

--- src/hierarchical-data/get-childrens.php
+++ src/hierarchical-data/get-childrens.php
@@ -5,13 +5,16 @@ $parent = $entityManager->find("\Wdm\Model\Structure", 1);
 
 get_childrens($parent);
 
-function get_childrens($parent)
+function get_childrens($parent, $level = 0)
 {
     if (!$parent) return;
 
     foreach ($parent->getChildrens() as $child) {
+        foreach (range(0, $level) as $l) {
+            echo "    ";
+        }
         echo $child->getName() . PHP_EOL;
-        get_childrens($child);
+        get_childrens($child, $level+1);
     }
 }
 
